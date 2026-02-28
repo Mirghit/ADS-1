eeefbbabb89f0a9da5b429ac870752b16c745f9f
Mirghit <yuandru25@gmail.com>
2026-02-28 16:48:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,60 +7,50 @@ bool checkPrime(uint64_t value) {
   if (value < 2){
     return false;
   }
-  
-  for(uint64_t i = 2; i <= value; i++) {
-    if(i != value && value % i == 0) {
-        return false;
+  for (uint64_t i = 2; i <= value; i++) {
+    if (i != value && value%i == 0) {
+      return false;
     }
   }
-  
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   uint64_t count = 0;
   uint64_t currentNumber = 1;
-
   while (count < n) {
     currentNumber++;
     if (checkPrime(currentNumber)) {
       count++;
     }
   }
-  
   return currentNumber;
 }
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t nextPrimeNumber = value + 1;
-  
   while (checkPrime(nextPrimeNumber) == false) {
     nextPrimeNumber++;
   }
-  
   return nextPrimeNumber;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  
-  for(uint64_t i = 2; i <= hbound; i++) {
-    if(checkPrime(i)) {
-        sum += i;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
     }
   }
-  
   return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t twins = 0;
-  
   for (uint64_t i = lbound; i < hbound - 2; i++) {
     if (checkPrime(i) && checkPrime(i+2)) {
       twins++;
     }
   }
-  
   return twins;
 }
